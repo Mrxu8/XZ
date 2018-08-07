64bf3eb35e86d784894352d50f2c3fbd7a972686
xu <772861588@qq.com>
2018-08-08 00:21:21 +0800

    second 1.3
    Signed-off-by: xu <772861588@qq.com>

--- src/main/java/com/oracle/xz/controll/NotesControll.java
+++ src/main/java/com/oracle/xz/controll/NotesControll.java
@@ -1,7 +1,19 @@
 package com.oracle.xz.controll;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * 游记controll
  */
+
+@Controller
+@RequestMapping(value = "/notes")
 public class NotesControll {
+
+    @RequestMapping(value = "/notesComment")
+    public String NotesComment(){
+        System.out.println("12");
+        return "comment";
+    }
 }
